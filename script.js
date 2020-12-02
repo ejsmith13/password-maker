9ca3584f33ebea05917f4de0a9b735f26115b641
ejsmith13 <ejsmithwork@gmail.com>
2020-12-01 21:51:17 -0500

    Questions

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function askQuestions() {
+
+  var confirmUpper = confirm("Would you like to use upper case letters?")
+  var confirmLower = confirm("Would you like to use lower case letters?")
+  var confirmNum = confirm("Would you like to use numbers?" )
+  var confirmSpecial= confirm("Would you like to use special characters?")
+  var promptLenght = prompt("How many characters would you like your password to have?")
+
+  if (confirmUpper) {
+    console.log("Uppercase")
+  }
+}
+
+askQuestions();
